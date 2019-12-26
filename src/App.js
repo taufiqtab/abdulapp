bf3af4bd4a291ab5d3ac8e7009a2eb4f79efecb8
taufiqtab <taufiqtab77@gmail.com>
2019-12-26 17:08:24 +0700

    Todo Apps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      todoItem : '',
+      items : []
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem:''
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.todoItem} onChange={this.handleChange}></input>
+            <button>Tambah</button>
+          </form>
+
+          <List items={this.state.items} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
