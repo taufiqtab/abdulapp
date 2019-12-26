f39f3fbeb0cbeed9e56acd93d9faedf69e54dc26
taufiqtab <taufiqtab77@gmail.com>
2019-12-26 17:43:58 +0700

    gogogo

--- src/ApiCoba.js
+++ src/ApiCoba.js
@@ -5,18 +5,23 @@ class ApiCoba extends Component{
     constructor(props){
         super(props)
         this.state = {
-            items:[]
+            items:[],
+            isLoading : true
         }
     }
 
     componentDidMount(){
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json())
-        .then(data => this.setState({items:data}))
+        .then(data => this.setState({items:data, isLoading :false}))
     }
 
     render(){
-        const {items} = this.state
+        const {items, isLoading} = this.state
+
+        if(isLoading){
+            return <p>LOADING....</p>
+        }
 
         return(
             <div>
