ee47529f25ad1e4fbfa75563c93feb2929049a05
taufiqtab <taufiqtab77@gmail.com>
2019-12-27 17:38:00 +0700

    test

--- src/component/DetailUsers.js
+++ src/component/DetailUsers.js
@@ -19,6 +19,22 @@ class DetailUsers extends Component {
         return (
             <div>
             <h2>Hai {this.state.user}</h2>
+            
+      <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <div class="card-panel grey lighten-5 z-depth-1">
+          <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="https://thumbnail.imgbin.com/4/11/12/imgbin-steven-ogg-grand-theft-auto-v-xbox-360-trevor-philips-franklin-clinton-grand-theft-auto-5-JjP29V1BxAK8eegvHqckgsVjL_t.jpg" alt="" class="circle responsive-img"/>
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                Ummm. Yummy Let's Party !!!!.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+            
           </div>
         );
     }
